92f1eadf24581d4161feb254aa5a090920bae199
Nikita <pl_an777@list.ru>
2026-06-25 16:12:06 +0700

    Добавьте файлы проекта.

---
+++ 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace _1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Random rnd = new Random();
+
+            Console.WriteLine("Введите размер матрицы");
+
+            int total = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int[,] array = new int[total, total];
+
+            for (int i = 0; i < total; i++)
+            {
+                for (int j = 0; j < total; j++)
+                {
+                    int number = rnd.Next(0, 100);
+                    if (number <= 70)
+                    {
+                        array[i, j] = 1;
+                    }
+                }
+            }
+            for (int r = 0; r < total; r++)
+            {
+                for (int c = 0; c < total; c++)
+                {
+                    if 
+                }
+            }
+
+                    for (int r = 0; r < total; r++)
+            {
+                for (int c = 0; c < total; c++)
+                {
+                    Console.Write(array[r, c] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+}
